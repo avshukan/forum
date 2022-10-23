eb0ea4dff6491d093ce3afec27fee64733ab439a
Alexander Shukan <avshukan@gmail.com>
2022-10-23 11:36:23 +0600

    add: uesrname input focus

--- client/src/components/LogButton.jsx
+++ client/src/components/LogButton.jsx
@@ -1,17 +1,26 @@
-import React, { useState, useCallback } from 'react';
+import React, {
+  useState, useEffect, useCallback, useRef,
+} from 'react';
 import { Button, Form, InputGroup } from 'react-bootstrap';
 import { useAuth } from '../contexts/AuthProvider';
 
 function LogButton() {
   const { username, logIn, logOut } = useAuth();
 
+  const ref = useRef();
+
   const [usernameValue, setUsernameValue] = useState('');
 
   const [editMode, setEditMode] = useState(false);
 
+  const onLogInClick = () => {
+    setEditMode(true);
+    ref.current.focus();
+  };
+
   const logOutButton = <Button onClick={() => logOut()}>LogOut</Button>;
 
-  const logInButton = <Button onClick={() => setEditMode(true)}>LogIn</Button>;
+  const logInButton = <Button onClick={onLogInClick}>LogIn</Button>;
 
   const button = useCallback(() => (username ? logOutButton : logInButton), [username]);
 
@@ -26,11 +35,16 @@ function LogButton() {
     setEditMode(false);
   };
 
+  useEffect(() => {
+    ref.current?.focus();
+  }, [editMode]);
+
   const edit = (
     <Form onSubmit={onSubmit}>
       <InputGroup className="mb-3">
         <InputGroup.Text id="basic-addon1">Set username:</InputGroup.Text>
         <Form.Control
+          ref={ref}
           placeholder="Username"
           aria-label="Username"
           aria-describedby="basic-addon1"
