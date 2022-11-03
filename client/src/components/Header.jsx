0e544de4f88aefe5f88738a9392e44c5c2aa829d
Alexander Shukan <avshukan@gmail.com>
2022-11-03 13:23:23 +0600

    add: fix header design

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
-import { Nav, Navbar, NavItem } from 'react-bootstrap';
+import {
+  Container, Nav, Navbar, NavItem, NavLink,
+} from 'react-bootstrap';
 import LogButton from './LogButton';
 import { useAuth } from '../contexts/AuthProvider';
 import CreatePostButton from './CreatePostButton';
@@ -9,30 +11,37 @@ function Header() {
   const { username } = useAuth();
 
   return (
-    <Navbar expand="lg" className="container App-header px-0">
-      <Nav>
-        <NavItem>
-          <CreatePostButton />
-        </NavItem>
-      </Nav>
-      <Nav className="ms-auto">
-        <NavItem>
-          {username || 'Unlogged user'}
-        </NavItem>
-      </Nav>
-      <Nav>
-        <NavItem>
-          <span className="px-3">
-            <img src={username ? '/images/default.jpg' : '/images/unknown.png'} className="img-fluid avatar-header rounded-circle shadow" alt="img" />
-          </span>
-        </NavItem>
-      </Nav>
-      <Nav>
-        <NavItem>
-          <LogButton />
-        </NavItem>
-      </Nav>
-    </Navbar>
+    <Container fluid className="App-header-wrapper">
+      <Navbar expand="lg" className="container App-header px-0">
+        <Nav>
+          <NavItem>
+            <CreatePostButton />
+          </NavItem>
+        </Nav>
+        <Nav>
+          <NavItem>
+            <NavLink href="https://github.com/avshukan/forum">Repo</NavLink>
+          </NavItem>
+        </Nav>
+        <Nav className="ms-auto">
+          <NavItem>
+            {username || 'Unlogged user'}
+          </NavItem>
+        </Nav>
+        <Nav>
+          <NavItem>
+            <span className="px-3">
+              <img src={username ? '/images/default.jpg' : '/images/unknown.png'} className="img-fluid avatar-header rounded-circle shadow" alt="img" />
+            </span>
+          </NavItem>
+        </Nav>
+        <Nav>
+          <NavItem>
+            <LogButton />
+          </NavItem>
+        </Nav>
+      </Navbar>
+    </Container>
   );
 }
 
--- client/src/components/Main.jsx
+++ client/src/components/Main.jsx
@@ -8,21 +8,23 @@ import Sidebar from './Sidebar';
 
 function Main() {
   return (
-    <Container className="d-flex flex-column h-100 App-area mx-auto">
+    <>
       <Header />
-      <Brand />
-      <Container className="px-0">
-        <Row>
-          <Col lg="8" sm="12" className="mb-5">
-            <Forum />
-          </Col>
-          <Col lg="4" sm="12">
-            <Sidebar />
-          </Col>
-        </Row>
+      <Container className="d-flex flex-column h-100 mx-auto px-0">
+        <Brand />
+        <Container className="px-0">
+          <Row>
+            <Col lg="8" sm="12" className="mb-5">
+              <Forum />
+            </Col>
+            <Col lg="4" sm="12">
+              <Sidebar />
+            </Col>
+          </Row>
+        </Container>
+        <Footer />
       </Container>
-      <Footer />
-    </Container>
+    </>
   );
 }
 
