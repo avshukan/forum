ceff4b978f04afba81e79fb3035d943f56f2ffab
Alexander Shukan <avshukan@gmail.com>
2022-11-19 14:04:50 +0600

    try: without cookie plugin

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -81,60 +81,63 @@ async function login(request, reply) {
     this.log.info({ message: 'header Content-Type' });
 
     this.log.info({ message: 'before cookie' });
+    reply.header('set-cookie', 'token=token');
+    reply.header('set-cookie', 'token12=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjp7ImlkIjo1LCJ1c2VybmFtZSI6IkFsaWNlIn0sImlhdCI6MTY2ODg0NDM4M30.k5QaMVal7_hmEI681EbnQX_LJUucimcTO6jtVJO8GHg; Domain=http://localhost; Path=/');
+    /*
+        reply
+          .setCookie('token01', token, { path: '/' })
+          .setCookie('token02', token, { domain: 'http://localhost:5000', path: '/' })
+          .setCookie('token03', token, { domain: 'http://localhost:5000/', path: '/' })
+          .setCookie('token04', token, { domain: 'http://localhost:3000', path: '/' })
+          .setCookie('token05', token, { domain: 'http://localhost:3000/', path: '/' })
+          .setCookie('token06', token, { domain: 'http://127.0.0.1:5000', path: '/' })
+          .setCookie('token07', token, { domain: 'http://127.0.0.1:5000/', path: '/' })
+          .setCookie('token08', token, { domain: 'http://127.0.0.1:3000', path: '/' })
+          .setCookie('token09', token, { domain: 'http://127.0.0.1:3000/', path: '/' })
 
-    reply
-      .setCookie('token01', token, { path: '/' })
-      .setCookie('token02', token, { domain: 'http://localhost:5000', path: '/' })
-      .setCookie('token03', token, { domain: 'http://localhost:5000/', path: '/' })
-      .setCookie('token04', token, { domain: 'http://localhost:3000', path: '/' })
-      .setCookie('token05', token, { domain: 'http://localhost:3000/', path: '/' })
-      .setCookie('token06', token, { domain: 'http://127.0.0.1:5000', path: '/' })
-      .setCookie('token07', token, { domain: 'http://127.0.0.1:5000/', path: '/' })
-      .setCookie('token08', token, { domain: 'http://127.0.0.1:3000', path: '/' })
-      .setCookie('token09', token, { domain: 'http://127.0.0.1:3000/', path: '/' })
+          .setCookie('token12', token, { domain: 'http://localhost', path: '/' })
+          .setCookie('token13', token, { domain: 'http://localhost/', path: '/' })
+          .setCookie('token14', token, { domain: 'http://localhost', path: '/' })
+          .setCookie('token15', token, { domain: 'http://localhost/', path: '/' })
+          .setCookie('token16', token, { domain: 'http://127.0.0.1', path: '/' })
+          .setCookie('token17', token, { domain: 'http://127.0.0.1/', path: '/' })
+          .setCookie('token18', token, { domain: 'http://127.0.0.1', path: '/' })
+          .setCookie('token19', token, { domain: 'http://127.0.0.1/', path: '/' })
 
-      .setCookie('token12', token, { domain: 'http://localhost', path: '/' })
-      .setCookie('token13', token, { domain: 'http://localhost/', path: '/' })
-      .setCookie('token14', token, { domain: 'http://localhost', path: '/' })
-      .setCookie('token15', token, { domain: 'http://localhost/', path: '/' })
-      .setCookie('token16', token, { domain: 'http://127.0.0.1', path: '/' })
-      .setCookie('token17', token, { domain: 'http://127.0.0.1/', path: '/' })
-      .setCookie('token18', token, { domain: 'http://127.0.0.1', path: '/' })
-      .setCookie('token19', token, { domain: 'http://127.0.0.1/', path: '/' })
+          .setCookie('token21', token, { domain: 'forum.avshukan.ru', path: '/' })
+          .setCookie('token22', token, { domain: 'forum-api.avshukan.ru', path: '/' })
+          .setCookie('token23', token, { domain: 'http://forum.avshukan.ru', path: '/' })
+          .setCookie('token24', token, { domain: 'http://forum-api.avshukan.ru', path: '/' })
+          .setCookie('token25', token, { domain: 'http://forum.avshukan.ru:3000', path: '/' })
+          .setCookie('token26', token, { domain: 'http://forum-api.avshukan.ru:3000', path: '/' })
+          .setCookie('token27', token, { domain: 'http://forum.avshukan.ru:5000', path: '/' })
+          .setCookie('token28', token, { domain: 'http://forum-api.avshukan.ru:5000', path: '/' })
 
-      .setCookie('token21', token, { domain: 'forum.avshukan.ru', path: '/' })
-      .setCookie('token22', token, { domain: 'forum-api.avshukan.ru', path: '/' })
-      .setCookie('token23', token, { domain: 'http://forum.avshukan.ru', path: '/' })
-      .setCookie('token24', token, { domain: 'http://forum-api.avshukan.ru', path: '/' })
-      .setCookie('token25', token, { domain: 'http://forum.avshukan.ru:3000', path: '/' })
-      .setCookie('token26', token, { domain: 'http://forum-api.avshukan.ru:3000', path: '/' })
-      .setCookie('token27', token, { domain: 'http://forum.avshukan.ru:5000', path: '/' })
-      .setCookie('token28', token, { domain: 'http://forum-api.avshukan.ru:5000', path: '/' })
-
-      .setCookie('token31', token, { domain: 'forum.avshukan.ru/', path: '/' })
-      .setCookie('token32', token, { domain: 'forum-api.avshukan.ru/', path: '/' })
-      .setCookie('token33', token, { domain: 'http://forum.avshukan.ru/', path: '/' })
-      .setCookie('token34', token, { domain: 'http://forum-api.avshukan.ru/', path: '/' })
-      .setCookie('token35', token, { domain: 'http://forum.avshukan.ru:3000/', path: '/' })
-      .setCookie('token36', token, { domain: 'http://forum-api.avshukan.ru:3000/', path: '/' })
-      .setCookie('token37', token, { domain: 'http://forum.avshukan.ru:5000/', path: '/' })
-      .setCookie('token38', token, { domain: 'http://forum-api.avshukan.ru:5000/', path: '/' })
-      .setCookie('a', 'a', {
-        httpOnly: true,
-      })
-      .setCookie('a3000', 'a3000', {
-        domain: 'localhost:3000',
-        path: '/',
-      })
-      .setCookie('a5000', 'a5000', {
-        domain: 'localhost:5000',
-        path: '/',
-      })
-      .setCookie('l', 'l', {
-        domain: '.localhost',
-        path: '/',
-      })
-      .cookie('baz', 'baz');
+          .setCookie('token31', token, { domain: 'forum.avshukan.ru/', path: '/' })
+          .setCookie('token32', token, { domain: 'forum-api.avshukan.ru/', path: '/' })
+          .setCookie('token33', token, { domain: 'http://forum.avshukan.ru/', path: '/' })
+          .setCookie('token34', token, { domain: 'http://forum-api.avshukan.ru/', path: '/' })
+          .setCookie('token35', token, { domain: 'http://forum.avshukan.ru:3000/', path: '/' })
+          .setCookie('token36', token, { domain: 'http://forum-api.avshukan.ru:3000/', path: '/' })
+          .setCookie('token37', token, { domain: 'http://forum.avshukan.ru:5000/', path: '/' })
+          .setCookie('token38', token, { domain: 'http://forum-api.avshukan.ru:5000/', path: '/' })
+          .setCookie('a', 'a', {
+            httpOnly: true,
+          })
+          .setCookie('a3000', 'a3000', {
+            domain: 'localhost:3000',
+            path: '/',
+          })
+          .setCookie('a5000', 'a5000', {
+            domain: 'localhost:5000',
+            path: '/',
+          })
+          .setCookie('l', 'l', {
+            domain: '.localhost',
+            path: '/',
+          })
+          .cookie('baz', 'baz');
+    */
 
     this.log.info({ message: 'before send' });
 
