d8917f2361dee229da6cedabf8526e9ba6ece395
Alexander Shukan <avshukan@gmail.com>
2022-11-28 12:43:18 +0600

    add: google config for oauth

--- server/plugins/oauth.js
+++ server/plugins/oauth.js
@@ -1,8 +1,29 @@
 const fp = require('fastify-plugin');
 const oauthPlugin = require('@fastify/oauth2');
 
+const { GOOGLE_ID, GOOGLE_SECRET, GOOGLE_CALLBACK_URI } = process.env;
+const { GITHUB_ID, GITHUB_SECRET, GITHUB_CALLBACK_URI } = process.env;
+
 module.exports = fp(async (fastify, _opts) => {
   // https://www.npmjs.com/package/@fastify/oauth2
+
+  fastify.register(oauthPlugin, {
+    name: 'googleOAuth2',
+    credentials: {
+      client: {
+        id: GOOGLE_ID,
+        secret: GOOGLE_SECRET,
+      },
+      auth: oauthPlugin.GOOGLE_CONFIGURATION,
+    },
+    scope: ['profile', 'email'],
+    // register a fastify url to start the redirect flow
+    startRedirectPath: '/auth/google/oauth2',
+    // facebook redirect here after the user login
+    // callbackUri: 'http://localhost:3000/login/facebook/callback',
+    callbackUri: GOOGLE_CALLBACK_URI,
+  });
+
   fastify.register(oauthPlugin, {
     name: 'facebookOAuth2',
     credentials: {
@@ -12,9 +33,11 @@ module.exports = fp(async (fastify, _opts) => {
       },
       auth: oauthPlugin.FACEBOOK_CONFIGURATION,
     },
+    scope: ['profile', 'email'],
     // register a fastify url to start the redirect flow
     startRedirectPath: '/login/facebook',
     // facebook redirect here after the user login
     callbackUri: 'http://localhost:3000/login/facebook/callback',
   });
+
 });
